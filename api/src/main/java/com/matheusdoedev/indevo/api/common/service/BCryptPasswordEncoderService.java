995e028c6f1650efddaf727c8745c81e332c2a46
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-09-01 11:03:34 -0300

    feat: bcrypt password encoder service implemented

---
+++ api/src/main/java/com/matheusdoedev/indevo/api/common/service/BCryptPasswordEncoderService.java
@@ -0,0 +1,17 @@
+package com.matheusdoedev.indevo.api.common.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BCryptPasswordEncoderService {
+	private BCryptPasswordEncoder bcrypt;
+
+	public BCryptPasswordEncoderService() {
+		this.bcrypt = new BCryptPasswordEncoder();
+	}
+
+	public BCryptPasswordEncoder getBcrypt() {
+		return this.bcrypt;
+	}
+}
