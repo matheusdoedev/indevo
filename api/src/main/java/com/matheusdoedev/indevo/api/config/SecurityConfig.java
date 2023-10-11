1cbeead2bc77402319a315210869efe6055a5475
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-10-11 10:04:45 -0300

    feat: it implements login and register endpoints in auth controller

--- api/src/main/java/com/matheusdoedev/indevo/api/config/SecurityConfig.java
+++ api/src/main/java/com/matheusdoedev/indevo/api/config/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.matheusdoedev.indevo.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +22,20 @@ public class SecurityConfig {
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+						.requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
 						.anyRequest().authenticated())
 				.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
--- api/src/main/java/com/matheusdoedev/indevo/api/controller/AuthController.java
+++ api/src/main/java/com/matheusdoedev/indevo/api/controller/AuthController.java
@@ -1,12 +1,17 @@
 package com.matheusdoedev.indevo.api.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheusdoedev.indevo.api.domain.auth.AuthenticationDto;
+import com.matheusdoedev.indevo.api.domain.user.dto.CreateUserResponseDto;
+import com.matheusdoedev.indevo.api.domain.user.dto.UserDto;
+import com.matheusdoedev.indevo.api.service.impl.UserServiceImpl;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -20,9 +25,26 @@ import lombok.AllArgsConstructor;
 @Tag(name = "authentication")
 public class AuthController {
 
+	private final AuthenticationManager authenticationManager;
+
+	private final UserServiceImpl userService;
+
 	@PostMapping("/login")
 	public ResponseEntity<String> login(@RequestBody @Valid AuthenticationDto authenticationDto) {
-		return ResponseEntity.ok("Ok");
+		UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(
+				authenticationDto.getUsername(),
+				authenticationDto.getPassword());
+
+		this.authenticationManager.authenticate(usernamePassword);
+
+		return ResponseEntity.ok().build();
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<CreateUserResponseDto> register(@RequestBody @Valid UserDto userDto) {
+		CreateUserResponseDto createdUser = this.userService.createUser(userDto);
+
+		return ResponseEntity.ok(createdUser);
 	}
 
 }
