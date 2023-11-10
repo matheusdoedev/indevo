19278a5612275ed9f7a433dffb8a4490c23b978e
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-11-10 16:24:24 -0300

    feat: it implements animation on scroll in header

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
-import { ReactNode } from 'react'
+'use client'
+
+import { ReactNode, useEffect } from 'react'
 
 import { Logo } from '@/components'
+import { CTA_BUTTON } from '@/constants'
 
 import './Header.styles.scss'
 
@@ -8,12 +11,36 @@ interface HeaderProps {
   children?: ReactNode
 }
 
-export default function Header({ children }: HeaderProps) {
+export default function Header({ children }: Readonly<HeaderProps>) {
+  useEffect(() => {
+    const header = document.getElementById('header') as HTMLElement
+
+    const handleAnimationOnScroll = () => {
+      const scrollTop = document.documentElement.scrollTop
+      const headerRect = header.getBoundingClientRect()
+
+      if (scrollTop > headerRect.height) {
+        header.classList.add('header--scroll-animation')
+      } else {
+        header.classList.remove('header--scroll-animation')
+      }
+    }
+
+    window.addEventListener('scroll', handleAnimationOnScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleAnimationOnScroll)
+    }
+  }, [])
+
   return (
-    <header className="header__wrapper">
+    <header id="header" className="header__wrapper">
       <div className="container header__container">
         <Logo />
-        {!!children && children}
+        <div className="header__content">
+          {!!children && children}
+          {CTA_BUTTON}
+        </div>
       </div>
     </header>
   )
