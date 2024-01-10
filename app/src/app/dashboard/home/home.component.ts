3958025974b6cf2db06817cc24678a043ab32f34
matheusdoedev <matheusdoe.dev@gmail.com>
2024-01-10 11:59:56 -0300

    feat(app): implementing create PDI plan modal

--- app/src/app/dashboard/home/home.component.ts
+++ app/src/app/dashboard/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  createNewPDIForm: FormGroup;
+  createNewPDIModalIsVisible: boolean = true;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.createNewPDIForm = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      thumbUrl: [''],
+    });
+  }
+
+  handleOpenNewPDIModal() {
+    this.createNewPDIModalIsVisible = true;
+  }
 }
