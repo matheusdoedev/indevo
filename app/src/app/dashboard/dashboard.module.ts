a40748527bc4bfd382246053143297f6199dd613
matheusdoedev <matheusdoe.dev@gmail.com>
2024-01-10 11:57:55 -0300

    feat(app): dashboard layout component created

--- app/src/app/dashboard/dashboard.module.ts
+++ app/src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { HomeComponent } from './home/home.component';
 import { DashboardModuleRouting } from './dashboard-routing.module';
+import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [DashboardModuleRouting, CommonModule],
+  declarations: [HomeComponent, DashboardLayoutComponent],
+  imports: [DashboardModuleRouting, CommonModule, SharedModule, FormsModule],
 })
 export class DashboardModule {}
